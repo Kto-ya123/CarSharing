0b7138687557f10826aee042f7465a6e2a672c16
Kto_ya123 <artur.luzgin.luzgin@mail.ru>
2020-02-07 23:38:19 +0300

    add MainController

---
+++ src/main/java/org/example/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(Model model){
+        return "greeting";
+    }
+}
---
+++ src/main/resources/templates/parts/template/greeting.ftl
@@ -0,0 +1,18 @@
+<#import "../style.ftl" as s>
+<div>
+    <H2>Car-Sharing</H2>
+</div>
+<div>
+    <a href="/car">Автомобиль</a>
+</div>
+<div>
+    <a href="/client">Клиент</a>
+</div>
+<div>
+    <a href="/contract">Контракт</a>
+</div>
+<div>
+    <a href="/accident">ДТП</a>
+</div>
+
+
