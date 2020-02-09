e14f6cad3829310d6c398f57367a12688a873ed7
victoria <vika.zhak1999@gmail.com>
2020-02-09 03:58:50 +0300

    few changes in MainController

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-
-    @GetMapping("/")
+    @GetMapping(value = "/")
     public String mainPage(Model model){
-        return "greeting";
+        return "start";
     }
 }
