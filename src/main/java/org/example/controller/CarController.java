206b9d9ccdd35c1ed8322d40b58adc50c4b61a85
Artur <artur.luzgin.luzgin@mail.ru>
2020-02-20 14:01:09 +0300

    init angular

--- src/main/java/org/example/controller/CarController.java
+++ src/main/java/org/example/controller/CarController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.entity.Car;
 import org.example.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/cars")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class CarController {
     private final CarService carService;
 
@@ -47,6 +49,19 @@ public class CarController {
                                @RequestParam Double price) {
         carService.update(id, model, color, bodyType, transmission, vehicleNumber, isFree, price);
         return "redirect:/cars";
+    }
+
+    @GetMapping("/get")
+    public @ResponseBody Car getCarInJSON(){
+        Car car = new Car();
+        car.setModel("Lexus");
+        car.setColor("White");
+        car.setBodyType("Minivan");
+        car.setTransmission("Manual");
+        car.setVehicleNumber("0708 EK-2");
+        car.setIsFree(Boolean.parseBoolean("false"));
+        car.setPrice(800.0);
+        return car;
     }
 
 }
